6eda24d27d2e4a22a8e2e44715f55071c5353f47
RickBirchall <40692376+RickBirchall@users.noreply.github.com>
2018-07-12 15:47:33 +0100

    adding comment

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ var server = http.createServer(function(request, response) {
 
 var port = process.env.PORT || 1337;
 server.listen(port);
-
+//example comment
 console.log("Server running at http://localhost:%d", port);
